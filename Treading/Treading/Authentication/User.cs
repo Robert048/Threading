3b1af3cd4e54d2d87f5192c428e8b45c2bfe9910
hakobk <hakobk1@gmail.com>
2016-03-29 13:01:54 +0200

    User

---
+++ Treading/Treading/Authentication/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treading.Authentication
+{
+    class User
+    {
+        public int userid;
+        public string username;
+        public string password;
+        public string mail;
+    }
+}

f91159d2c577a9365b028e08026f180146a668a5
robert048 <robert>
2016-04-04 11:59:53 +0200

    objects

---
+++ PictureSharing/PictureSharing/Foto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSharing
+{
+    class Foto
+    {
+        private long fotoID;
+        private string fotoNaam;
+        private long gebruikersID;
+        private string path;
+    }
+}
---
+++ PictureSharing/PictureSharing/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSharing
+{
+    class User
+    {
+        private long gebruikersID;
+        private string gebruikersNaam;
+        private string gebruikersPW;
+    }
+}
